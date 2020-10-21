1750bd076deaefe713a2e83bca9f0cbf8676c90f
Sammaljihad <jsammal69@gmail.com>
2020-10-21 15:13:40 +0700

    first commit

---
+++ Folder 2/Praktikum 1/kenalan.php
@@ -0,0 +1,10 @@
+<?php
+class kenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Sammal Jihad<br>
+                saya tinggal di Slawi<br>
+                saya suka olahraga Berenang";
+    }
+
+}
