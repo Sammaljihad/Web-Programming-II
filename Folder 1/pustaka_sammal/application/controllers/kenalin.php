a8f409fdeea784707e8038869de0dc0e44fd48f9
Sammaljihad <55831231+Sammaljihad@users.noreply.github.com>
2020-10-28 20:38:16 +0700

    Update kenalin.php

--- Folder 1/pustaka_sammal/application/controllers/kenalin.php
+++ Folder 1/pustaka_sammal/application/controllers/kenalin.php
@@ -2,9 +2,9 @@
 class kenalin extends CI_Controller{
     public function index() {
         echo "<h1> perkenalan</h1>";
-        echo "Muhammad Hanif Aji Wiratama<br>
-                saya tinggal di Pagerbarang<br>
-                saya suka olahraga futsal";
+        echo "Sammal Jihad<br>
+                saya tinggal di slawi<br>
+                saya suka olahraga renang";
     }
 
 }
